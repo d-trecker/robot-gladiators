cdec80763239272f66ab2419443e551c9bc2b3be
d trec <dylan.j.trecker@gmail.com>
2021-03-29 04:09:09 -0700

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,27 +1,56 @@
 var playerName = window.prompt("What is your robot's name?");
-console.log (playerName);
-console.log (10+10);
-console.log ("Our robots name is " + playerName)
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
 
-function fight() {
-    window.alert("The fight has begun!");
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Sans";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle?");
+    console.log (promptFight);
+    //if player choses to fight
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(
+            playerName + " attacked " + enemyName + ". " +enemyName + "now has" + enemyHealth + " remaining."
+        );
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    } else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    playerHealth = playerHealth - enemyAttack;
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    } else { 
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+    
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        if (confirmSkip) {
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            playerMoney = playerMoney -2;
+        }
+
+        else {
+            fight();
+        }
+
+
+    }
 }
 
-//fight();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// comment
-/* long 
-comment */
+fight();
